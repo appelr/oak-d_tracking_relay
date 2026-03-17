ef792f6ba032e5a3506255fb442dfdf7de455e75
appelr <appelrandolf@gmail.com>
2026-03-17 10:17:45 +0100

    Flipped frame for hand presence detection to be consistent for unity

--- src/oakd_tracking_relay/tracking_manager.py
+++ src/oakd_tracking_relay/tracking_manager.py
@@ -246,6 +246,9 @@ class HandTracker:
 
     def process(self, frame):
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+
+        # Frame flip, da Resultate sonst gespiegelt sind
+        frame_rgb = cv2.flip(frame_rgb, 1)
         results = self.model.process(frame_rgb)
         
         found_upper_left = False
